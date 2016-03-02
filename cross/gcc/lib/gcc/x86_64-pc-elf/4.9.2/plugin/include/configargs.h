1662576a28e0342e6608d94673fe5aeaaad7b604
shshunda <shshunda@zone29-ea.cs.ucalgary.ca>
2016-03-01 17:42:30 -0700

    Fresh a3

---
+++ cross/gcc/lib/gcc/x86_64-pc-elf/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --target=x86_64-pc-elf --prefix=/usr/local/kos/cross/gcc --enable-languages=c,c++ --enable-lto --disable-nls --disable-tls --with-newlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
